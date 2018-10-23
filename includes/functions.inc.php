346aeae5a42cad148c83a32c6a96b5378fd8744f
RomainDessaint <romaindessaint@gmail.com>
2018-10-23 13:41:10 +0200

    connection ok

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -9,6 +9,7 @@ function db_connection() {
 	if ($connection->connect_error) {
 	    die("Connection failed: " . $connection->connect_error);
 	}
+	return $connection;
 }
 
 /*-----------------------------------------------------------------------------*/
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	include('includes/display.inc.php');
 	include('includes/functions.inc.php');
+	require 'includes/conf_db.inc.php';
 ?>
 
 <html lang = "fr">
@@ -17,11 +18,37 @@
 		<h1> COMMUNITY LIFE MANAGER </h1>
 
 		<?php
-		db_connection();
+
+		require 'includes/conf_db.inc.php';
+		//création de la connexion
+		$connection = new mysqli($host, $login, $password, $dbname);
+		//vérification de la connexion
+		if ($connection->connect_error) {
+		    die("Connection failed: " . $connection->connect_error);
+		}
+
+		if ($connection->connect_errno) {
+    			printf("Connect failed: %s\n", $connection->connect_error);
+    			exit();
+		}
+
 		echo connectionForm();
+
 		if(isset($_POST['connect'])) {
 			if($_POST['login'] != "" && $_POST['password'] != "") {
-				echo('<p style="color: red;"> Connexion. </p>');
+				$login = $_POST['login'];
+				$password = $_POST['password'];
+				//$query = "SELECT * FROM users";
+				$query = "SELECT * FROM users WHERE login = '$login' AND pass = '$password'";
+				$result = $connection->query($query);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+				  		echo('<p style="color: red;"> Identifiants corrects. </p>');
+					}
+				} else {
+					echo('<p style="color: red;"> Identifiants incorrects. </p>');
+				}
+				$connection->close();
 			}
 			else {
 				echo('<p style="color: red;"> Veuillez renseigner vos identifiants svp. </p>');
