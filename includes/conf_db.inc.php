1d98e52734d923b7fa76807f46de308188110079
RomainDessaint <romaindessaint@gmail.com>
2018-11-01 15:07:39 +0100

    local database parameters OK host = 'localhost' port = '3306' dbname = 'db_clm' login = 'root' password = ''

--- includes/conf_db.inc.php
+++ includes/conf_db.inc.php
@@ -2,17 +2,17 @@
 	//Paramètres de connexion à la base de données
 
 /*-------------Base de données UCP-------------*/
-	$host = '10.40.128.23';
-	$port = '3306';
-	$dbname = 'db2018l3idwa_rdessain';
-	$login = 'y18l3i_rdessain';
-	$password = 'A123456*';
+	// $host = '10.40.128.23';
+	// $port = '3306';
+	// $dbname = 'db2018l3idwa_rdessain';
+	// $login = 'y18l3i_rdessain';
+	// $password = 'A123456*';
 
 /*-----------Base de données locale------------*/
-	// $host = 'localhost';
-	// $port = '3306';
-	// $dbname = '';
-	// $login = '';
-	// $password = '';
+	$host = 'localhost';
+	$port = '3306';
+	$dbname = 'db_clm';
+	$login = 'root';
+	$password = '';
 
 ?>
