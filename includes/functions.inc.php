4217afe52da1945371c8c50d0bba514b219c1309
TimotheeS <timothee95@hotmail.fr>
2018-11-09 11:03:50 +0100

    l

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -19,7 +19,7 @@ function db_connection() {
 //fonction d'affichage du formulaire de connexion
 function connectionForm() {
 	$return = '<form action = "#" method = "POST">';
-	$return .= '<table>';
+	$return .= '<table class = "centered bordered" style="margin-top: 10px;">';
 	$return .= '<tr>';
 	$return .= '<td class="nb" colspan=2> <h3> Connectez-vous </h3> </td>';
 	$return .= '</tr> <tr>';
@@ -29,7 +29,7 @@ function connectionForm() {
 	$return .= '<td> Mot de passe : </td>';
 	$return .= '<td> <input type="password" name="password" autocomplete="off"> </td>';
 	$return .= '</tr> <tr>';
-	$return .= '<td class="nb" colspan=2> <input type="submit" value="Se connecter" name="connect"> </td>';
+	$return .= '<td class="nb" colspan=2> <input type="submit" value="Se connecter" name="log_in"> </td>';
 	$return .= '</tr>';
 	$return .= '</table>';
 	$return .= '</form>';
@@ -39,7 +39,7 @@ function connectionForm() {
 //fonction de vérification des identifiants/mots de passe dans la base de données
 function connection($connection) {
 	$return = null;
-	if(isset($_POST['connect'])) {
+	if(isset($_POST['log_in'])) {
 		if($_POST['login'] != "" && $_POST['password'] != "") {
 			$login = $_POST['login'];
 			$password = $_POST['password'];
@@ -48,6 +48,16 @@ function connection($connection) {
 			if ($result->num_rows > 0) {
 				while($row = $result->fetch_assoc()) {
 					$return = '<p style="color: red;"> Identifiants corrects. </p>';
+					session_start();
+					$_SESSION['user_connected'] = true;
+	                    $_SESSION['user_login'] = $login;
+					$query = "SELECT user_name, user_forename FROM users WHERE login = '$login' AND pass = '$password'";
+					$result = $connection->query($query);
+					while($row = $result->fetch_assoc()) {
+						$_SESSION['user_name'] = $row['user_name'];
+						$_SESSION['user_forename'] = $row['user_forename'];
+					}
+					header('location: index.php');
 				}
 			} else {
 				$return = '<p style="color: red;"> Identifiants incorrects. </p>';
@@ -66,10 +76,50 @@ function connection($connection) {
 			}
 		}
 	}
-	$return = '<table> <tr> <td>' .$return .'</td> </tr> </table>';
+	$return = '<table class = "centered"> <tr> <td>' .$return .'</td> </tr> </table>';
+	return $return;
+}
+
+function sessionInformation() {
+	$return = '<table>';
+	$return .= '<tr>';
+	if(!isset($_SESSION['user_connected'])) {
+		$return .= '<td class = "nb"> Vous n\'etes pas connecté. </td>';
+	} else if(isset($_SESSION['user_connected']) && $_SESSION['user_connected'] == true) {
+		$return .= '<td class = "nb"> Connecté en tant que : </td>';
+		$return .= '<td class = "nb">' .$_SESSION['user_forename'] .' ' .$_SESSION['user_name'] .'</td>';
+		$return .= '<td class = "nb"> <form action="pages/log_out.php" method="POST"> <input type="submit" value="Se déconnecter" name="log_out"> </form> </td>';
+	}
+	$return .= '</tr>';
+	$return .= '</table>';
 	return $return;
 }
 
 /*-----------------------------------------------------------------------------*/
+
+function createOrganizationForm($connection) {
+	$return = '<form action="#" method="post">';
+	$return .= '<table>';
+	$return .= '<tr>';
+	$return .= '<td colspan=2> <h3> Créer mon association </h3> </td>';
+	$return .= '</tr> <tr>';
+	$return .= '<td> Nom de l\'assocation : </td> <td> <input type="text" style="width: 400px;"> </td>';
+	$return .= '</tr> <tr>';
+	$return .= '<td> Description de l\'assocation : </td> <td> <textarea style="height: 80px; width: 400px;"> </textarea> </td>';
+	$return .= '</tr> <tr>';
+	$return .= '<td> Séléctionnez votre école : </td> <td> <select value="" name="school" style="width: 400px;">';
+	$return .= '<option valu ="default" selected=selected> ... </option>';
+	$query = "SELECT school_name FROM schools ORDER BY school_name";
+	$result = $connection->query($query);
+	while($row = $result->fetch_assoc()) {
+		// $temp = $row["school_name"];
+		$return .= '<option name="' .$row['school_name'] .'" value="' .$row['school_name'] .'">' .$row['school_name'] .'</option>';
+	}
+	$return .= '</select> </td>';
+	$return .= '</tr>';
+	$return .= '</table>';
+	$return .= '</form>';
+	return $return;
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 	include('includes/display.inc.php');
 	include('includes/functions.inc.php');
-	require 'includes/conf_db.inc.php';
+
+	session_start();
 ?>
 
 <html lang = "fr">
@@ -18,45 +19,28 @@
 		<h1> COMMUNITY LIFE MANAGER </h1>
 
 		<?php
-<<<<<<< HEAD
-			// Create connection //
-			$connection = new mysqli($host, $login, $password, $dbname);
-			// Check connection
-			if ($connection->connect_error) {
-			    die("Connection failed: " . $connection->connect_error);
-			}
+		$connection = db_connection();
 
-			$query = "SELECT * FROM users";
-			$result = $connection->query($query);
-
-			if ($result->num_rows > 0) {
-    				// output data of each row
-    				while($row = $result->fetch_assoc()) {
-        				echo "Role: " . $row["role"]. "<br/> Login: " . $row["login"]. "<br/> Password: " . $row["pass"]. "<br/>";
-    				}
-			} else {
-    				echo "0 results";
-			}
-=======
-
-		require 'includes/conf_db.inc.php';
-		//création de la connexion
-		$connection = new mysqli($host, $login, $password, $dbname);
-		//vérification de la connexion
-		if ($connection->connect_error) {
-		    die("Connection failed: " . $connection->connect_error);
-		}
-
-		if ($connection->connect_errno) {
-    			printf("Connect failed: %s\n", $connection->connect_error);
-    			exit();
-		}
-
-		echo connectionForm();
-		echo connection($connection);
-
->>>>>>> 10b2ece5e9c0410d5940fb4ee029e7f5a6942b2d
 		?>
+
+		<div id = "banner">
+			<?php
+				echo sessionInformation();
+			?>
+		</div>
+
+		<section>
+			<?php
+			echo createOrganizationForm($connection);
+			?>
+		</section>
+
+		<aside>
+			<?php
+			echo connectionForm();
+			echo connection($connection);
+			?>
+		</aside>
 	</body>
 
 </html>
